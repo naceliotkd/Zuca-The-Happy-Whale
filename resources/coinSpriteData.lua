1e7d02c60a7fa862c3b92964896ace40fb3e978c
naceliotkd <nacelio@gmail.com>
2014-05-12 20:19:16 -0300

    ..

---
+++ resources/coinSpriteData.lua
@@ -0,0 +1,65 @@
+local coinSpriteData = {}
+coinSpriteData.frames = {
+	{
+		x = 1,
+		y = 0,
+		width = 21,
+		height = 21
+	},
+	{
+		x = 24,
+		y = 0,
+		width = 21,
+		height = 21
+	},
+	{
+		x = 47,
+		y = 0,
+		width = 21,
+		height = 21
+	},
+	{
+		x = 71,
+		y = 0,
+		width = 21,
+		height = 21
+	},
+	{
+		x = 93,
+		y = 0,
+		width = 21,
+		height = 21
+	},
+	{
+		x = 116,
+		y = 0,
+		width = 21,
+		height = 21
+	},
+	{
+		x = 139,
+		y = 0,
+		width = 21,
+		height = 21
+	},
+	{
+		x = 163,
+		y = 0,
+		width = 21,
+		height = 21
+	},
+	{
+		x = 186,
+		y = 0,
+		width = 21,
+		height = 21
+	},
+	{
+		x = 210,
+		y = 0,
+		width = 21,
+		height = 21
+	}
+}
+
+return coinSpriteData
